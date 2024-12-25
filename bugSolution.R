b7172ffdfd170ebdfdfa3c1cb88263f23b52b847
BugHunter-X <askzenai@gmail.com>
2024-12-25 01:12:06 +0100

    Add bugSolution.R file

---
+++ bugSolution.R
@@ -0,0 +1,6 @@
+```R
+# This code demonstrates how to handle missing values (NA) when calculating the mean.
+my_vector <- c(1, 2, 3, NA, 5)
+mean_result <- mean(my_vector, na.rm = TRUE) # na.rm = TRUE removes NA values before calculation
+print(mean_result)
+```
